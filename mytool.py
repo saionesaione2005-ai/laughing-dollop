71fdadd4de10ff36d94f0432841d6bac4d18731b
Zaa <saionesaione2005@gmail.com>
2026-07-10 19:22:28 +0800

    Add initial version of mytool.py

---
+++ mytool.py
@@ -0,0 +1,11 @@
+import time
+
+def main():
+    print("--- ကျွန်ုပ်၏ ပထမဆုံး Tool ---")
+    print("စနစ္ကို စစ္ေဆးေနသည္...")
+    time.sleep(1)
+    print("ေအာင္ျမင္စြာ စတင္လိုက္ပါၿပီ!")
+    print("Welcome to my tool!")
+
+if __name__ == "__main__":
+    main()
